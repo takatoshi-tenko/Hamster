e84111fc076bf9c3e5bd9c465e157c5f7d5007e1
takatoshi.tenko <chanten811@gmail.com>
2024-02-12 20:39:05 +0900

    Initial Commit

---
+++ Hamster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hamster
+//
+//  Created by 天孝高俊 on 2024/02/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
