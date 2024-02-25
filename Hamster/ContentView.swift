201f5b959bbef7172ff5588a74ab9a165bfc89d6
takatoshi.tenko <chanten811@gmail.com>
2024-02-25 18:07:26 +0900

    ボタンを押したらアラートが出るようにした

--- Hamster/ContentView.swift
+++ Hamster/ContentView.swift
@@ -6,38 +6,26 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct ContentView: View {
+    @State private var showAlert = false
     var body: some View {
         VStack {
-            MapView()
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                
-                Divider()
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
+            Button("他の端末と連携する") {
+                showAlert.toggle()
             }
-            .padding()
-            Spacer()
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("連携する"), message: Text("他の端末と連携する"))
+            }
         }
-        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
 
 #Preview {
