fc6e103b2538d36edde8d1f10708931d0948383a
takatoshi.tenko <chanten811@gmail.com>
2024-02-25 21:31:14 +0900

    misc

---
+++ Hamster/Views/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Hamster
+//
+//  Created by 天孝高俊 on 2024/02/25.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
