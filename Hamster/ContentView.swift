91911a2a8d45ddba2f0c05400702f094acc43e86
takatoshi.tenko <chanten811@gmail.com>
2024-02-25 22:07:18 +0900

    不要なファイルの削除

---
+++ Hamster/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Hamster
+//
+//  Created by 天孝高俊 on 2024/02/12.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
