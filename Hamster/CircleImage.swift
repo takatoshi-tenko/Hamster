6146132f8f90f4ed79e92a2d08b2f110bc12efb2
takatoshi.tenko <chanten811@gmail.com>
2024-02-25 17:28:08 +0900

    MapViewの追加

---
+++ Hamster/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Hamster
+//
+//  Created by 天孝高俊 on 2024/02/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
