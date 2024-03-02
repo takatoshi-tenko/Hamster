f6e2ef474b68df0ccba7d76910c8a277c889e1cd
takatoshi.tenko <chanten811@gmail.com>
2024-03-02 21:26:57 +0900

    一旦最初の状態に戻した

--- Hamster/ContentView.swift
+++ Hamster/ContentView.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
     }
 }
 
---
+++ Hamster/HamsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamsterApp.swift
+//  Hamster
+//
+//  Created by 天孝高俊 on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+struct HamsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
